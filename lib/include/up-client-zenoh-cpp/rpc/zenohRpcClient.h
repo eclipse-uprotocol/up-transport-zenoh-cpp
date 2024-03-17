12b432989d4c2dc7fc13e5743e7daaaf6e273977
Michael Melamud <melamudmichael@gmail.com>
2024-03-17 17:55:30 +0000

    adding function description

--- lib/include/up-client-zenoh-cpp/rpc/zenohRpcClient.h
+++ lib/include/up-client-zenoh-cpp/rpc/zenohRpcClient.h
@@ -73,6 +73,18 @@ class ZenohRpcClient : public uprotocol::rpc::RpcClient {
                                                               const uprotocol::utransport::UPayload &payload, 
                                                               const uprotocol::v1::CallOptions &options) noexcept;
 
+        /**
+        * API for clients to invoke a method (send an RPC request) and receive the response (the returned 
+        * {@link CompletionStage} {@link UPayload}. <br>
+        * Client will set method to be the URI of the method they want to invoke, 
+        * payload to the request message, and attributes with the various metadata for the 
+        * method invocation.
+        * @param methodUri The method URI to be invoked, ex (long form): /example.hello_world/1/rpc.SayHello.
+        * @param requestPayload The request message to be sent to the server.
+        * @param options RPC method invocation call options, see {@link CallOptions}
+        * @param callback that will be called once the future is complete
+        * @return UStatus
+        */
         uprotocol::v1::UStatus invokeMethod(const uprotocol::v1::UUri &topic,
                                             const uprotocol::utransport::UPayload &payload,
                                             const uprotocol::v1::CallOptions &options,
